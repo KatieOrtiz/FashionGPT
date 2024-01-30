eb7e7bd954f9f3416fff50460c69fa2a9b22b60e
AliRaza-CSUN <ali.raza.780@my.csun.edu>
2024-01-29 20:05:13 -0800

    first push of the year

--- app.py
+++ app.py
@@ -8,6 +8,21 @@ def index():
 @app.route('/emailVerification')
 def emailVerification():
     return render_template('emailVerification.html')
+@app.route('/homePage')
+def emailVerification():
+    return render_template('homePage.html')
+@app.route('/PasswordPage')
+def emailVerification():
+    return render_template('PasswordPage.html')
+@app.route('/personalDetails')
+def emailVerification():
+    return render_template('personalDetails.html')
+@app.route('/register')
+def emailVerification():
+    return render_template('register.html')
+@app.route('/sizePreference')
+def emailVerification():
+    return render_template('sizePreference.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
--- static/script.js
+++ static/script.js
@@ -2,11 +2,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("getStarted");
   button.addEventListener("click", function () {
-    window.location.href = "emailVerification";
+    window.location.href = "emailVerification.html";
   });
   var button = document.getElementById("getStarted2");
   button.addEventListener("click", function () {
-    window.location.href = "emailVerification";
+    window.location.href = "emailVerification.html";
   });
 });
 
@@ -14,6 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   var button = document.getElementById("backBtn");
   button.addEventListener("click", function () {
-    window.location.href = "frontPage";
+    window.location.href = "frontPage.html";
   });
 });
+//homepage
+document.addEventListener('DOMContentLoaded', function () {
+    const coll = document.querySelectorAll('.collapsible');
+
+    coll.forEach(function (item) {
+        item.addEventListener('click', function () {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+});
+
